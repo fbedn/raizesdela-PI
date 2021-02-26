44c9f277fb0a121ccaa379e886102de0d09f0f47
fbedn <f_ramos@outlook.com>
2021-02-26 16:40:03 -0300

    feat(controller/produto):adicionando put ao controller do produto

--- raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/controller/ProdutoController.java
+++ raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,11 @@ public class ProdutoController {
 	@PostMapping
 	public ResponseEntity<Produto> post(@RequestBody Produto produto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@RequestBody Produto produto) {
+		return ResponseEntity.ok(repository.save(produto));
 	}
 	
 }
