fd9a0ecc7dd7ec63c02b102452549c3a92526baa
fbedn <f_ramos@outlook.com>
2021-02-26 14:48:04 -0300

    feat(controller/produto):criando controller do produto e adicionando findAll ao controller

---
+++ raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/controller/ProdutoController.java
@@ -0,0 +1,26 @@
+package br.com.raizesdela.raizesDela.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.raizesdela.raizesDela.model.Produto;
+import br.com.raizesdela.raizesDela.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+}
