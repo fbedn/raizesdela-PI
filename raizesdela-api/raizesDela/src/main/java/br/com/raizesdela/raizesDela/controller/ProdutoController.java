b9e4961a11b88521f60e3982487a66ee957456e7
fbedn <f_ramos@outlook.com>
2021-02-26 16:44:28 -0300

    feat(controller/produto):adicionando delete ao controller do produto

--- raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/controller/ProdutoController.java
+++ raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,11 @@ public class ProdutoController {
 	@PutMapping
 	public ResponseEntity<Produto> put(@RequestBody Produto produto) {
 		return ResponseEntity.ok(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 	
 }
