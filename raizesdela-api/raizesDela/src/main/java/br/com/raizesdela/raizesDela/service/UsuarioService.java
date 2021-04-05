f04897b28fe555d8abac1a0565046ad9f675a755
Lucas <lucasloiolaalmeida@gmail.com>
2021-04-05 13:08:23 -0300

    fix(UsurarioService/BasicSecurityConfig): implementando autorização de rotas para cadastro de produtos e atualização de cadastros

--- raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/service/UsuarioService.java
+++ raizesdela-api/raizesDela/src/main/java/br/com/raizesdela/raizesDela/service/UsuarioService.java
@@ -9,44 +9,45 @@ import br.com.raizesdela.raizesDela.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 @Service
 public final class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	public Usuario CadastrarUsuario(Usuario usuario) {
-		if(repository.findAllByEmailContainingIgnoreCase(usuario.getEmail()).isPresent()) {
+		if (repository.findAllByEmailContainingIgnoreCase(usuario.getEmail()).isPresent() && usuario.getId() == 0) {
 			return null;
 		}
-		
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
+
 		String senhaEncoder = encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
-		
+
 		return repository.save(usuario);
 	}
-	
-	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user){
+
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findAllByEmailContainingIgnoreCase(user.get().getEmail());
-		
-		if(usuario.isPresent()) {
-			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
-				
-			String auth = user.get().getEmail()+ ":" + user.get().getSenha();
-			byte[] encondedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-			String authHeader = "Basic "+ new String(encondedAuth);
-			user.get().setToken(authHeader);
-			user.get().setNome(usuario.get().getNome());
-			user.get().setTipoVendedor(usuario.get().getTipoVendedor());
-			user.get().setId(usuario.get().getId());
-			
-			return user;
-			
+
+		if (usuario.isPresent()) {
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = user.get().getEmail() + ":" + user.get().getSenha();
+				byte[] encondedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encondedAuth);
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				user.get().setTipoVendedor(usuario.get().getTipoVendedor());
+				user.get().setId(usuario.get().getId());
+
+				return user;
+
 			}
 		}
-		
+
 		return null;
 	}
 }
